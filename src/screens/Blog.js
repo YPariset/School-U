3925d2c25e28c6caccf0583ff889cbf91f951ce8
Stratfab <fabsiegele@gmail.com>
2021-04-07 12:09:50 +0200

    Structure Blog_Page

--- src/screens/Blog.js
+++ src/screens/Blog.js
@@ -1,43 +1,104 @@
-import React from 'react'
-import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
-import Button from '../components/Button'
-import Paragraph from '../components/Paragraph'
+import React, { Component } from 'react'
+import { Image, StyleSheet } from 'react-native'
 import {
   Container,
+  Header,
   Content,
   Card,
   CardItem,
   Thumbnail,
   Text,
+  Button,
   Icon,
   Left,
   Body,
-  Right,
+  Footer,
 } from 'native-base'
-import { View, StyleSheet, Image } from 'react-native'
-
-export default function Blog() {
-  return (
-    <Background>
-      <View>
-        <Header>Blog</Header>
-        <View style={styles.icon}>
+export default class Blog extends Component {
+  render() {
+    return (
+      <Container style={styles.fond}>
+        <Header />
+        <Content>
+          <Card style={{ flex: 0 }}>
+            <CardItem>
+              <Body>
+                <Text>Anniversaire Lucie</Text>
+                <Text note>April 15, 2016</Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Image
+                  source={require('../assets/anniversaireenfant.jpeg')}
+                  style={{ height: 200, width: 400, flex: 1 }}
+                />
+                <Text>C'était trop bien !!!!!!</Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent>
+                  <Icon active name="thumbs-up" />
+                  <Text>12 Likes</Text>
+                </Button>
+              </Left>
+            </CardItem>
+          </Card>
+          <Card style={{ flex: 0 }}>
+            <CardItem>
+              <Body>
+                <Text>Chanson</Text>
+                <Text note>April 15, 2016</Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                  Au clair de la lune {'\n'}
+                  {'\n'}Au clair de la Lune {'\n'}Mon ami Pierrot{'\n'}
+                  Prêtes-moi ta plume {'\n'}Pour écrire un mot {'\n'}Ma
+                  chandelle est morte
+                  {'\n'}Je n'ai plus de feu {'\n'}Ouvres-moi ta porte {'\n'}Pour
+                  l'amour de Dieu
+                  {'\n'}Au clair de la Lune {'\n'}Pierrot répondit {'\n'}Je n'ai
+                  pas de plume Je suis dans mon lit {'\n'}Vas chez la voisine{' '}
+                  {'\n'}Je crois qu'elle y est
+                  {'\n'}Car dans sa cuisine On bat le briquet{'\n'}
+                </Text>
+                <Text>J'adore ! 🥰</Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent>
+                  <Icon active name="thumbs-up" />
+                  <Text>8 Likes</Text>
+                </Button>
+              </Left>
+            </CardItem>
+          </Card>
+        </Content>
+        <Footer style={styles.icon}>
           <Icon style={styles.plus} name="add-circle-outline"></Icon>
-        </View>
-      </View>
-    </Background>
-  )
+        </Footer>
+      </Container>
+    )
+  }
 }
 const styles = StyleSheet.create({
-  icon: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
+  fond: {
+    backgroundColor: '#fff9eb',
   },
 
   plus: {
     fontSize: 50,
+  },
+
+  icon: {
+    backgroundColor: '#fff9eb',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
   },
 })
