389f733f62f274551982c6ddc0ef45e8bdc7086c
YPariset <yoann.pariset@edu.itescia.fr>
2021-04-11 15:11:48 +0200

    firestore auth

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,125 +1,194 @@
-import React, { useState } from 'react'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
-import { Text } from 'react-native-paper'
-import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
-import Button from '../components/Button'
-import TextInput from '../components/TextInput'
-import BackButton from '../components/BackButton'
-import { theme } from '../core/theme'
-import { emailValidator } from '../helpers/emailValidator'
-import { passwordValidator } from '../helpers/passwordValidator'
-import { nameValidator } from '../helpers/nameValidator'
-import { signUpUser } from '../api/auth-api'
-import Toast from '../components/Toast'
-import Switch from '../components/Switch'
+import React from "react";
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Image, StatusBar } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import UserPermissions from "../helpers/UserPermissions";
+import * as ImagePicker from "expo-image-picker";
+import Fire from "../core/Fire";
 
-export default function RegisterScreen({ navigation }) {
-  const [name, setName] = useState({ value: '', error: '' })
-  const [email, setEmail] = useState({ value: '', error: '' })
-  const [password, setPassword] = useState({ value: '', error: '' })
-  const [loading, setLoading] = useState()
-  const [error, setError] = useState()
+export default class RegisterScreen extends React.Component {
+    static navigationOptions = {
+        header: null
+    };
 
-  const onSignUpPressed = async () => {
-    const nameError = nameValidator(name.value)
-    const emailError = emailValidator(email.value)
-    const passwordError = passwordValidator(password.value)
-    if (emailError || passwordError || nameError) {
-      setName({ ...name, error: nameError })
-      setEmail({ ...email, error: emailError })
-      setPassword({ ...password, error: passwordError })
-      return
+    state = {
+        user: {
+            name: "",
+            email: "",
+            password: "",
+            avatar: null
+        },
+        errorMessage: null
+    };
+
+    handleSignUp = () => {
+        Fire.shared.createUser(this.state.user);
+    };
+
+    handlePickAvatar = async () => {
+        UserPermissions.getCameraPermission();
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3]
+        });
+
+        if (!result.cancelled) {
+            this.setState({ user: { ...this.state.user, avatar: result.uri } });
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle="light-content"></StatusBar>
+                <Image
+                    source={require("../assets/authHeader.png")}
+                    style={{ marginTop: -116, marginLeft: -50 }}
+                ></Image>
+                <Image
+                    source={require("../assets/authFooter.png")}
+                    style={{ position: "absolute", bottom: -325, right: -225 }}
+                ></Image>
+                <TouchableOpacity style={styles.back} onPress={() => this.props.navigation.goBack()}>
+                    <Ionicons name="ios-arrow-round-back" size={32} color="#FFF"></Ionicons>
+                </TouchableOpacity>
+                <View style={{ position: "absolute", top: 64, alignItems: "center", width: "100%" }}>
+                    <Text style={styles.greeting}>{`Hello!\nSign up to get started.`}</Text>
+                    <TouchableOpacity style={styles.avatarPlaceholder} onPress={this.handlePickAvatar}>
+                        <Image source={{ uri: this.state.user.avatar }} style={styles.avatar} />
+                        <Ionicons
+                            name="ios-add"
+                            size={40}
+                            color="#FFF"
+                            style={{ marginTop: 6, marginLeft: 2 }}
+                        ></Ionicons>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.errorMessage}>
+                    {this.state.errorMessage && <Text style={styles.error}>{this.state.errorMessage}</Text>}
+                </View>
+
+                <View style={styles.form}>
+                    <View>
+                        <Text style={styles.inputTitle}>Full Name</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={name => this.setState({ user: { ...this.state.user, name } })}
+                            value={this.state.user.name}
+                        ></TextInput>
+                    </View>
+
+                    <View style={{ marginTop: 32 }}>
+                        <Text style={styles.inputTitle}>Email Address</Text>
+                        <TextInput
+                            style={styles.input}
+                            autoCapitalize="none"
+                            onChangeText={email => this.setState({ user: { ...this.state.user, email } })}
+                            value={this.state.user.email}
+                        ></TextInput>
+                    </View>
+
+                    <View style={{ marginTop: 32 }}>
+                        <Text style={styles.inputTitle}>Password</Text>
+                        <TextInput
+                            style={styles.input}
+                            secureTextEntry
+                            autoCapitalize="none"
+                            onChangeText={password => this.setState({ user: { ...this.state.user, password } })}
+                            value={this.state.user.password}
+                        ></TextInput>
+                    </View>
+                </View>
+
+                <TouchableOpacity style={styles.button} onPress={this.handleSignUp}>
+                    <Text style={{ color: "#FFF", fontWeight: "500" }}>Sign up</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={{ alignSelf: "center", marginTop: 32 }}
+                    onPress={() => this.props.navigation.navigate("Login")}
+                >
+                    <Text style={{ color: "#414959", fontSize: 13 }}>
+                        Already have an account? <Text style={{ fontWeight: "500", color: "#E9446A" }}>Sign in</Text>
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
     }
-    setLoading(true)
-    const response = await signUpUser({
-      name: name.value,
-      email: email.value,
-      password: password.value,
-    })
-    if (response.error) {
-      setError(response.error)
-    }
-    setLoading(false)
-  }
-
-  return (
-    <Background>
-      <BackButton goBack={navigation.goBack} />
-      <Logo />
-      <Header>Créer un compte</Header>
-      <View style={styles.container}>
-        <Text style={styles.textStyle}>This is an Example of Switch</Text>
-        <Switch>yo</Switch>
-        <Switch>yo</Switch>
-      </View>
-      
-      <TextInput
-        label="Name"
-        returnKeyType="next"
-        value={name.value}
-        onChangeText={(text) => setName({ value: text, error: '' })}
-        error={!!name.error}
-        errorText={name.error}
-      />
-      <TextInput
-        label="Email"
-        returnKeyType="next"
-        value={email.value}
-        onChangeText={(text) => setEmail({ value: text, error: '' })}
-        error={!!email.error}
-        errorText={email.error}
-        autoCapitalize="none"
-        autoCompleteType="email"
-        textContentType="emailAddress"
-        keyboardType="email-address"
-      />
-      <TextInput
-        label="Password"
-        returnKeyType="done"
-        value={password.value}
-        onChangeText={(text) => setPassword({ value: text, error: '' })}
-        error={!!password.error}
-        errorText={password.error}
-        secureTextEntry
-      />
-      <Button
-        loading={loading}
-        mode="contained"
-        labelStyle={{ 
-          color: "white",
-          fontWeight: 'bold',
-          fontSize: 15,
-          lineHeight: 26,
-        }}
-        onPress={onSignUpPressed}
-        style={{ marginTop: 24 }}
-      >
-        Créer un compte
-      </Button>
-      <View style={styles.row}>
-        <Text>Vous avez déja un compte ? </Text>
-        <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
-          <Text style={styles.link}>Se connecter</Text>
-        </TouchableOpacity>
-      </View>
-      <Toast message={error} onDismiss={() => setError('')} />
-    </Background>
-  )
 }
-
 const styles = StyleSheet.create({
-  container:{
-    flexDirection: 'row',
-    alignItems: 'center', justifyContent: 'center',
+    container: {
+        flex: 1
     },
-  row: {
-    flexDirection: 'row',
-    marginTop: 4,
-  },
-  link: {
-    fontWeight: 'bold',
-    color: theme.colors.primary,
-  }
-})
+    greeting: {
+        marginTop: 32,
+        fontSize: 18,
+        fontWeight: "500",
+        textAlign: "center",
+        color: "#FFF"
+    },
+    form: {
+        marginBottom: 48,
+        marginHorizontal: 30
+    },
+    inputTitle: {
+        color: "#8A8F9E",
+        fontSize: 10,
+        textTransform: "uppercase"
+    },
+    input: {
+        borderBottomColor: "#8A8F9E",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        height: 40,
+        fontSize: 15,
+        color: "#161F3D"
+    },
+    button: {
+        marginHorizontal: 30,
+        backgroundColor: "#E9446A",
+        borderRadius: 4,
+        height: 52,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    errorMessage: {
+        height: 72,
+        alignItems: "center",
+        justifyContent: "center",
+        marginHorizontal: 30
+    },
+    error: {
+        color: "#E9446A",
+        fontSize: 13,
+        fontWeight: "600",
+        textAlign: "center"
+    },
+    back: {
+        position: "absolute",
+        top: 48,
+        left: 32,
+        width: 32,
+        height: 32,
+        borderRadius: 16,
+        backgroundColor: "rgba(21, 22, 48, 0.1)",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    avatarPlaceholder: {
+        width: 100,
+        height: 100,
+        backgroundColor: "#E1E2E6",
+        borderRadius: 50,
+        marginTop: 48,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    avatar: {
+        position: "absolute",
+        width: 100,
+        height: 100,
+        borderRadius: 50
+    }
+});
