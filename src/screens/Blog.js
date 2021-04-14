43fa94e8deed374fbd878c8363ad673e52bafdc2
Stratfab <fabsiegele@gmail.com>
2021-04-14 16:02:14 +0200

    first commit

--- src/screens/Blog.js
+++ src/screens/Blog.js
@@ -1,15 +1,13 @@
-import React from "react";
-import { View, Text, StyleSheet, Image, FlatList } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import moment from "moment";
+import React from 'react'
+import { View, Text, StyleSheet, Image, FlatList } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import moment from 'moment'
 import Button from '../components/Button'
-import Fire from "../core/Fire";
-import { db } from '../core/Fire';
+import Fire from '../core/Fire'
+import { db } from '../core/Fire'
 
-
-const firebase = require("firebase");
-require("firebase/firestore");
-
+const firebase = require('firebase')
+require('firebase/firestore')
 
 // temporary data until we pull from Firebase
 /*const posts = [
@@ -51,153 +49,171 @@ require("firebase/firestore");
     }
 ];*/
 
-
 export default class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.ref = Fire.shared.firestore.collection('posts')
+    this.useref = this.state = {
+      dataSource: [],
+    }
+  }
+  componentDidMount() {
+    this.feed = this.ref.onSnapshot(this.feedPosts)
+  }
 
-    constructor(props){
-        super(props)
-        this.ref =  Fire.shared.firestore.collection('posts')
-        this.useref=
-        this.state={
-          dataSource : []
-        }
-      
-      }
-      componentDidMount(){
-        this.feed = this.ref.onSnapshot(this.feedPosts);
-      }
-      
-      feedPosts = (postSnapShot) =>{
-        const post = [];
-        postSnapShot.forEach((doc) => {
-        const {uid,text,timestamp,image} = doc.data();
-        const data=Fire.shared.firestore
+  feedPosts = (postSnapShot) => {
+    const post = []
+    postSnapShot.forEach((doc) => {
+      const { uid, text, timestamp, image } = doc.data()
+      const data = Fire.shared.firestore
         .collection('users')
         .doc(uid)
         .get()
-        .then(doc=>{
+        .then((doc) => {
           post.push({
-            avatar:doc.data().avatar
-            ,name:doc.data().name,uid,
-            
+            avatar: doc.data().avatar,
+            name: doc.data().name,
+            uid,
+
             text,
             timestamp,
-            image
+            image,
           })
           this.setState({
-            dataSource : post,
-          });
+            dataSource: post,
+          })
         })
-        });
-      }
+    })
+  }
 
-    renderPost = post => {
-        return (
-            <View style={styles.feedItem}>
-                <Image source={{uri: post.avatar}} style={styles.avatar} />
-                <View style={{ flex: 1 }}>
-                    <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-                        <View>
-                            <Text style={styles.name}>{post.name}</Text>
-                            <Text style={styles.timestamp}>{moment(post.timestamp).fromNow()}</Text>
-                        </View>
-
-                        <Ionicons name="ellipsis-horizontal-outline" size={24} color="#73788B" />
-                    </View>
-                    <Text style={styles.post}>{post.text}</Text>
-                    <Image source={{uri: post.image}} style={styles.postImage} resizeMode="cover" />
-                    <View style={{ flexDirection: "row" }}>
-                        <Ionicons name="heart-outline" size={24} color="#73788B" style={{ marginRight: 16 }} />
-                        <Ionicons name="chatbox-outline" size={24} color="#73788B" />
-                    </View>
-                </View>
+  renderPost = (post) => {
+    return (
+      <View style={styles.feedItem}>
+        <Image source={{ uri: post.avatar }} style={styles.avatar} />
+        <View style={{ flex: 1 }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <View>
+              <Text style={styles.name}>{post.name}</Text>
+              <Text style={styles.timestamp}>
+                {moment(post.timestamp).fromNow()}
+              </Text>
             </View>
-        );
-    };
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <FlatList
-                    style={styles.feed}
-                    data={this.state.dataSource}
-                    renderItem={({ item }) => this.renderPost(item)}
-                    keyExtractor={item => item.id}
-                    showsVerticalScrollIndicator={false}
-                ></FlatList>
-                <View style={styles.header}>
-                <Button
-                  onPress={() => this.props.navigation.navigate('PostScreen')}
-                >
-                <Ionicons
-                  name="add-circle-outline"
-                  size={30}
-                  style={{ color: 'grey'}}
-                ></Ionicons>
-                </Button>
-              </View>
-            </View>
-        );
-    }
+            <Ionicons
+              name="ellipsis-horizontal-outline"
+              size={24}
+              color="#73788B"
+            />
+          </View>
+          <Text style={styles.post}>{post.text}</Text>
+          <Image
+            source={{ uri: post.image }}
+            style={styles.postImage}
+            resizeMode="cover"
+          />
+          <View style={{ flexDirection: 'row' }}>
+            <Ionicons
+              name="heart-outline"
+              size={24}
+              color="#73788B"
+              style={{ marginRight: 16 }}
+            />
+            <Ionicons name="chatbox-outline" size={24} color="#73788B" />
+          </View>
+        </View>
+      </View>
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          style={styles.feed}
+          data={this.state.dataSource}
+          renderItem={({ item }) => this.renderPost(item)}
+          keyExtractor={(item, index) => {
+            return index.toString()
+          }}
+          showsVerticalScrollIndicator={false}
+        ></FlatList>
+        <View style={styles.header}>
+          <Button onPress={() => this.props.navigation.navigate('PostScreen')}>
+            <Ionicons
+              name="add-circle-outline"
+              size={30}
+              style={{ color: 'grey' }}
+            ></Ionicons>
+          </Button>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#EBECF4"
-    },
-    header: {
-        height: 100,
-        backgroundColor: "#FFF",
-        alignItems: "center",
-        justifyContent: "center",
-        borderBottomWidth: 1,
-        borderBottomColor: "#EBECF4",
-        shadowColor: "#454D65",
-        shadowOffset: { height: 5 },
-        shadowRadius: 15,
-        shadowOpacity: 0.2,
-        zIndex: 1
-    },
-    headerTitle: {
-        fontSize: 50,
-        fontWeight: "800"
-    },
-    feed: {
-        marginHorizontal: 16
-    },
-    feedItem: {
-        backgroundColor: "#FFF",
-        borderRadius: 5,
-        padding: 8,
-        flexDirection: "row",
-        marginVertical: 8
-    },
-    avatar: {
-        width: 36,
-        height: 36,
-        borderRadius: 18,
-        marginRight: 16
-    },
-    name: {
-        fontSize: 15,
-        fontWeight: "500",
-        color: "#454D65"
-    },
-    timestamp: {
-        fontSize: 11,
-        color: "#C4C6CE",
-        marginTop: 4
-    },
-    post: {
-        marginTop: 16,
-        fontSize: 14,
-        color: "#838899"
-    },
-    postImage: {
-        width: undefined,
-        height: 150,
-        borderRadius: 5,
-        marginVertical: 16
-    }
-});
+  container: {
+    flex: 1,
+    backgroundColor: '#EBECF4',
+  },
+  header: {
+    height: 100,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#EBECF4',
+    shadowColor: '#454D65',
+    shadowOffset: { height: 5 },
+    shadowRadius: 15,
+    shadowOpacity: 0.2,
+    zIndex: 1,
+  },
+  headerTitle: {
+    fontSize: 50,
+    fontWeight: '800',
+  },
+  feed: {
+    marginHorizontal: 16,
+  },
+  feedItem: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    padding: 8,
+    flexDirection: 'row',
+    marginVertical: 8,
+  },
+  avatar: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    marginRight: 16,
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#454D65',
+  },
+  timestamp: {
+    fontSize: 11,
+    color: '#C4C6CE',
+    marginTop: 4,
+  },
+  post: {
+    marginTop: 16,
+    fontSize: 14,
+    color: '#838899',
+  },
+  postImage: {
+    width: undefined,
+    height: 150,
+    borderRadius: 5,
+    marginVertical: 16,
+  },
+})
